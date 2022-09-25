b4ee87b9eb585b45ad869f28f5541147f94fc5ef
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-09-25 19:50:41 +0300

    add style for table

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from './Statistics.module.css';
 
 export const Statistics = ({data, title}) => {
         return <section className={css.statistic} key="statistics">
-      {checkTitle(title)}
+      {(title) && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {data.map(item => 
       <li className={css.item} key={item.id} style={{backgroundColor : getRandomHexColor(), width: findWidth(data)}}>
@@ -13,9 +13,7 @@ export const Statistics = ({data, title}) => {
     </ul>
   </section>
 }
-function checkTitle(title){
-  if(title){return <h2 className={css.title}>{title}</h2>};
-}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
