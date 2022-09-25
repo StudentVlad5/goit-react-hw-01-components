f0b6e165bf7c1bfb739aefbdb7b864bb31d08314
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-09-25 07:39:27 +0300

    done task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../user.json';
 import data from '../data.json';
 import friend from '../friends.json';
+import transactions from '../transactions.json';
 
 
 export const App = () => {
@@ -22,6 +24,7 @@ export const App = () => {
     <Profile user = {user}/>
     <Statistics title="Upload stats" data = {data} />
     <FriendList friend = {friend} />
+    <TransactionHistory transactions = { transactions } />
     </div>
     
   );
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
-export const FriendList = ({friend}) => { return <section>
+export const FriendList = ({friend}) => { return <section className={css.sectionFriendList}>
 <ul className={css.friendList} key="friendList">
   {friend.map(item => <li className={css.items} key={item.id}>
   <span className={css.status} style={{backgroundColor: checkStatus(item.isOnline)}}>{item.isOnline}</span>
