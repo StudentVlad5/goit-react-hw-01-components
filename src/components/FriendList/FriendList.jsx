258b4ea80940578ec24e6c94561b62a164a2202d
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-10-25 22:04:27 +0300

    add FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,29 +1,15 @@
+import {FriendListItem}  from 'components/FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
-export const FriendList = ({friend}) => { return <section className={css.sectionFriendList}>
-<ul className={css.friendList} key="friendList">
-  {friend.map(item => <li className={css.items} key={item.id}>
-  <span className={css.status} style={{backgroundColor: checkStatus(item.isOnline)}}>{item.isOnline}</span>
-  <img className={css.avatar} src={item.avatar} alt="User avatar" width="48" />
-  <p className={css.name}>{item.name}</p>
-</li>
-  )}
+export const FriendList = ({friend}) => { return (<section className={css.sectionFriendList}>
+<ul className={css.friendList}>
+  {friend.map(item =><FriendListItem item = {item} key={item.id}/>)}
 </ul>
 </section>
-}
+)}
 
-function checkStatus (isOnline) {
-return isOnline? 'green':'red' 
-}
 
-checkStatus.propTypes = PropTypes.func
 FriendList.propTypes = {
     friend: PropTypes.array.isRequired,
-    item: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-})
 }
